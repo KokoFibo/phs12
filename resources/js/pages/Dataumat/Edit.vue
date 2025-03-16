899dc58dd409d66d696f215ec117291fed80c741
KokoFibo <kokonacci@gmail.com>
2025-03-16 10:48:22 +0700

    ok2

--- resources/js/pages/Dataumat/Edit.vue
+++ resources/js/pages/Dataumat/Edit.vue
@@ -392,7 +392,9 @@ function submit() {
 
                 <!-- Tombol Simpan -->
                 <div class="flex justify-between">
-                    <button @click="back" class="rounded bg-gray-900 px-4 py-2 text-white hover:bg-gray-400" :disabled="form.processing">Back</button>
+                    <button @click.prevent="back" class="rounded bg-gray-900 px-4 py-2 text-white hover:bg-gray-400" :disabled="form.processing">
+                        Back
+                    </button>
                     <button type="submit" class="rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600" :disabled="form.processing">
                         Update
                     </button>
